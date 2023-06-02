017736d44be91d947f8cd1ec8dc6d61205e0d1f5
bi0punk <systecinfo_cl@criptext.com>
2023-06-02 15:54:54 -0400

    test branch

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, jsonify
+import datetime
+from db import get_db_connection, create_table
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_sensor_data():
+    return render_template("index.html")
+
+
+
+@app.route('/sensor', methods=['POST'])
+def receive_sensor_data():
+    data = request.json
+    if data and 'temperature' in data:
+        temperature = float(data['temperature'])
+        save_sensor_data(temperature)
+        print(temperature)
+        check_temperature(temperature, 18.0)  # Cambia 18.0 por el valor límite que desees
+        return 'Datos del sensor recibidos'
+    return ''
+
+
+def save_sensor_data(temperature):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cursor.execute("INSERT INTO sensor_data (temperature, timestamp) VALUES (?, ?)", (temperature, current_datetime))
+    conn.commit()
+    conn.close()
+
+
+def fetch_sensor_data():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, temperature, timestamp FROM sensor_data")
+    sensor_data = [{'id': row['id'], 'temperature': row['temperature'], 'timestamp': row['timestamp']} for row in cursor.fetchall()]
+    conn.close()
+    return sensor_data
+
+
+def check_temperature(temperature, limit):
+    if temperature < limit:
+        print(f"La temperatura es más baja que {limit} grados")  # Cambia el mensaje según tus necesidades
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="192.168.1.218", port=5000)
---
+++ sensor-data/sensor-data.ino
@@ -0,0 +1,70 @@
+#include <Ethernet.h>
+#include <SPI.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ArduinoJson.h>
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 100);  // Dirección IP estática del Arduino
+IPAddress server(192, 168, 1, 87);
+EthernetClient client;
+
+unsigned long lastMillis = 0;
+const unsigned long interval = 20000;
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  Ethernet.begin(mac, ip);
+}
+
+void loop() {
+  if (millis() - lastMillis > interval) {
+    lastMillis = millis();
+
+    sensors.requestTemperatures();
+    float tempC = sensors.getTempCByIndex(0);
+
+    if (tempC != DEVICE_DISCONNECTED_C) {
+      Serial.print("Temperatura: ");
+      Serial.print(tempC);
+      Serial.println(" C");
+
+      // Crear un objeto JSON
+      StaticJsonDocument<64> jsonDocument;
+      jsonDocument["temperature"] = tempC;
+
+      // Serializar el objeto JSON en una cadena
+      String jsonString;
+      serializeJson(jsonDocument, jsonString);
+
+      Serial.println(jsonString);
+
+      if (client.connect(server, 5000)) {
+        Serial.println("Conectado al servidor");
+
+        client.print("POST /sensor HTTP/1.1\r\n");
+        client.print("Host: 192.168.1.129\r\n");
+        client.print("Connection: close\r\n");
+        client.print("Content-Type: application/json\r\n");
+        client.print("Content-Length: ");
+        client.print(jsonString.length());
+        client.print("\r\n\r\n");
+        client.print(jsonString);
+
+        Serial.println(jsonString);
+
+        Serial.println("Información enviada exitosamente");
+
+        client.stop();
+      } else {
+        Serial.println("Error de conexión");
+      }
+    }
+  }
+}
