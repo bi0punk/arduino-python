4edc0bf82629de62b8fde60bfe852c5c13b11b67
bi0punk <systecinfo_cl@criptext.com>
2023-06-06 14:24:42 -0400

    -

--- core/main.py
+++ core/main.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    sensor_data = fetch_ultimos_registros(50)  # Obtener los últimos 50 registro
-    return render_template("/home/index.html", sensor_data=sensor_data)
+    return render_template("/home/index.html", fetch_sensor_data=fetch_sensor_data)
 
 
 @app.route('/sensor', methods=['POST'])
@@ -48,20 +47,6 @@ def check_temperature(temperature, limit):
     if temperature < limit:
         text = f"Temperatura baja detectada, {limit} grados"
         print(text)
-
-
-@app.route('/ultimos_registros', methods=['GET'])
-def obtener_ultimos_registros():
-    sensor_data = fetch_ultimos_registros(50)  # Obtener los últimos 50 registros
-    return render_template("index.html", sensor_data=sensor_data)
-
-
-def fetch_ultimos_registros(num_registros):
-    with get_db_connection() as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, temperature, timestamp FROM sensor_data ORDER BY id DESC LIMIT ?", (num_registros,))
-        sensor_data = [{'id': row['id'], 'temperature': row['temperature'], 'timestamp': row['timestamp']} for row in cursor.fetchall()]
-    return sensor_data
 
 
 
