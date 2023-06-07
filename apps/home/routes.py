c096883d10f7a3b21d89e4ab9b890041e679e619
bi0punk <systecinfo_cl@criptext.com>
2023-06-07 02:37:22 -0400

    -

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,4 +1,3 @@
-
 from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
@@ -6,12 +5,14 @@ from jinja2 import TemplateNotFound
 
 
 @blueprint.route('/index')
+@login_required
 def index():
 
     return render_template('home/index.html', segment='index')
 
 
 @blueprint.route('/<template>')
+@login_required
 def route_template(template):
 
     try:
